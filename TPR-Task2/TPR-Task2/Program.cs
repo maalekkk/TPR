776f1124005c7af77270020e9f35a98dab041fe2
maalekkk <bartek.malkowski97@gmail.com>
2020-11-22 14:24:27 +0100

    create solution to task 2

---
+++ TPR-Task2/TPR-Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TPR_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

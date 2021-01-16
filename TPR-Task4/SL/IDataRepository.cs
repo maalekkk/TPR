ec7d60247847737bb60b8b3058e74b272cd6cce0
Young-oMusician <michal.bitnerowski@outlook.com>
2021-01-16 12:49:47 +0100

    Added IDataRepository

---
+++ TPR-Task4/SL/IDataRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL
+{
+    public interface IDataRepository
+    {
+        public delegate void OwnHandler();
+        public event OwnHandler OnRepositoryChange;
+        void AddLocation(short id, String name, decimal costRate, decimal avaibility, DateTime modifiedDate);
+        void UpdateLocation(short id, string name, decimal costRate, decimal avaibility, DateTime modifiedDate);
+        void DeleteLocation(short id);
+        List<short> GetLocationsIds();
+        String GetLocationName(short id);
+        decimal GetLocationCostRate(short id);
+        decimal GetLocationAvaibility(short id);
+        DateTime GetLocationModifiedDate(short id);
+    }
+}

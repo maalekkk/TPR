756cee7dcbd3cb1132cb5edb7045ebe9d51652dd
maalekkk <bartek.malkowski97@gmail.com>
2021-01-16 13:03:35 +0100

    add LocationDataContext.cs add GetLocationMethod

---
+++ TPR-Task4/Models/LocationDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public partial class LocationDataContext
+    {
+        public List<Location> GetAllLocations()
+        {
+            IQueryable<Location> query = from location in Locations
+                                         select location;
+
+            return query.ToList();
+        }
+    }
+}

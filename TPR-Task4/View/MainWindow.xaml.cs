e334cbebe59c8e62bfbcf9ff2e8f8632d8edddae
maalekkk <bartek.malkowski97@gmail.com>
2021-01-16 12:59:02 +0100

    add MainWindows.cs

---
+++ TPR-Task4/View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using ViewModel;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}

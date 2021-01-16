898de9755d53b7c2ba8e79cbee2a03898aec2ef7
Young-oMusician <michal.bitnerowski@outlook.com>
2021-01-16 17:06:25 +0100

    Added name validator

---
+++ TPR-Task4/View/Validators/DecimalValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validators
+{
+    class DecimalValidation : ValidationRule
+    {
+        public string Error { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(decimal.TryParse(value.ToString(), NumberStyles.AllowDecimalPoint, cultureInfo ,out decimal i))
+            {
+                
+                if(i >= 0)
+                {
+                    return new ValidationResult(true, null);
+                }
+                else
+                {
+                    Error = "Number must be positive!";
+                    return new ValidationResult(false, Error);
+                }
+            }
+            Error = "Invalid number format!";
+            return new ValidationResult(false, Error);
+        }
+    }
+}
---
+++ TPR-Task4/View/Validators/NameValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validators
+{
+    class NameValidation : ValidationRule
+    {
+        public string Error { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(value == null)
+            {
+                Error = "Name cannot be null!";
+                return new ValidationResult(false, Error);
+            }
+            if(value.ToString().Length == 0 || value.ToString().Length > 50)
+            {
+                Error = "Name should has <1;50> characters!";
+                return new ValidationResult(false, Error);
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}

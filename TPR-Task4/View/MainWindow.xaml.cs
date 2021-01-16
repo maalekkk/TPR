5590128c341dd2ef84e2904bb2939db6d1441d0f
maalekkk <bartek.malkowski97@gmail.com>
2021-01-16 12:59:33 +0100

    add OnInitialized method in MainWindow

--- TPR-Task4/View/MainWindow.xaml.cs
+++ TPR-Task4/View/MainWindow.xaml.cs
@@ -15,5 +15,11 @@ namespace View
             InitializeComponent();
         }
 
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            MainViewModel main = (MainViewModel)DataContext;
+        }
+
     }
 }

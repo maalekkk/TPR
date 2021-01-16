afad675db3f31f7c3d93b32746684f9f9c665ad8
maalekkk <bartek.malkowski97@gmail.com>
2021-01-16 12:42:45 +0100

    add interface to DataContext

---
+++ TPR-Task4/Models/IDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public interface IDataContext
+    {
+        void Add(Location location);
+        List<Location> GetAll();
+        Location Get(short id);
+        void Delete(Location location);
+        void Update(short id, Location location);
+    }
+}

b6b8a7bc9cd3a5d5340e1ed0e15d0caf2f6d74ac
maalekkk <bartek.malkowski97@gmail.com>
2021-01-16 17:44:55 +0100

    add DateTime validator

---
+++ TPR-Task4/View/Validators/DateTimeValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validators
+{
+    class DateTimeValidator : ValidationRule
+    {
+        public string Error { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            DateTime date;
+            try
+            {
+                date = DateTime.Parse(value.ToString(), cultureInfo);
+            }
+            catch
+            {
+                string Error = "Invalid date format!";
+                return new ValidationResult(false, Error);
+            }
+            if (date.Year > 1799 && date.Year <= DateTime.Today.Year)
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                Error = "Date must be between 1/1/1799 and 1/1/9999";
+                return new ValidationResult(false, Error);
+            }
+        }
+    }
+}
